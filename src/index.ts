4dfe5a2b9f4adfffa8b251ef09ac8adc3d6db38a
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-16 16:35:33 +0100

    add game access

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,22 @@ app.set("view engine", "njk");
 
 app.get("/", (request, response) => {
   apiCaller("http://videogame-api.fly.dev/platforms", (error, body) => {
-    if (error) throw error;
+    if (error) {
+      throw error;
+    }
     const plateform = JSON.parse(body);
-    console.log(plateform);
     response.render("home", { plateformsAcces: plateform.platforms });
+  });
+});
+
+app.get("/plateform", (request, response) => {
+  apiCaller(`http://videogame-api.fly.dev/games/platforms/19c9dcae-80a2-e137-50ff-11b823738827`, (error, body) => {
+    if (error) {
+      throw error;
+    }
+    const game = JSON.parse(body);
+    console.log(game);
+    response.render("plateform-game.njk", { gameAcces: game.games });
   });
 });
 
